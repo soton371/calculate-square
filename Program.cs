1c189de3422677882e3a968806618bda842d5df3
Soton <77579218+soton371@users.noreply.github.com>
2021-01-21 20:13:52 +0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Given the length of a side of a square, calculate and print its area..
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x*x);
+        }
+    }
+}
